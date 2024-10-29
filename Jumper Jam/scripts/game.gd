4afb380e2c2c21c2765ca6133441b5d11d5acad2
colin <beutt102@mail.chapman.edu>
2024-10-29 14:00:58 -0700

    fixed_camera_view

--- Jumper Jam/scripts/game.gd
+++ Jumper Jam/scripts/game.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+var camera_scene = preload("res://scenes/game_camera.tscn")
+
+var camera = null
+
+func _ready():
+	camera = camera_scene.instantiate()
+	camera.setup_camera($player)
+	add_child(camera)
+	
+
+func _process(delta):
+	pass
