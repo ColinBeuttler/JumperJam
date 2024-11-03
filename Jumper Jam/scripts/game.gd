02a6d0e3da2e4340c448ee2db361abcde17780bb
colin <beutt102@mail.chapman.edu>
2024-11-02 18:37:16 -0700

    gameComplete

--- Jumper Jam/scripts/game.gd
+++ Jumper Jam/scripts/game.gd
@@ -1,37 +1,71 @@
 extends Node2D
 
-@onready var platform_parent = $PlatformParent
+
+@onready var level_generator = $LevelGenerator
+@onready var ground_sprite = $GroundSprite
+@onready var parallax1 = $ParallaxBackground/ParallaxLayer
+@onready var parallax2 = $ParallaxBackground/ParallaxLayer2
+@onready var parallax3 = $ParallaxBackground/ParallaxLayer3
+
 
 var camera_scene = preload("res://scenes/game_camera.tscn")
-var platform_scene = preload("res://scenes/Platform.tscn")
+var player_scene = preload("res://scenes/player.tscn")
 
+var player: Player = null
+var player_spawn_pos: Vector2
 var camera = null
+var viewport_size: Vector2
+
 
 func _ready():
-	camera = camera_scene.instantiate()
-	camera.setup_camera($player)
-	add_child(camera)
+	viewport_size = get_viewport_rect().size
+	var player_spawn_pos_y_offset = 135
 	
-	# Generate the Ground
-	var viewport_size = get_viewport_rect().size
-	var platform_width = 136
-	var ground_layer_platform_count = (viewport_size.x / platform_width) + 1
+	player_spawn_pos.x = viewport_size.x / 2.0
+	player_spawn_pos.y = viewport_size.y - player_spawn_pos_y_offset
 	
-	var ground_layer_y_offset = 62
-	for i in range(ground_layer_platform_count):
-		var ground_location = Vector2(i * platform_width ,viewport_size.y - ground_layer_y_offset)
-		create_platorm(ground_location)
+	ground_sprite.global_position.x = viewport_size.x / 2.0
+	ground_sprite.global_position.y = viewport_size.y
+	
+	setup_parallax_layer(parallax1)
+	setup_parallax_layer(parallax2)
+	setup_parallax_layer(parallax3)
+	
+	new_game()
 
-func _process(delta):
+func get_parallax_sprite_scale(parallax_sprite: Sprite2D):
+	var parallax_texture = parallax_sprite.get_texture()
+	var parallax_texture_width = parallax_texture.get_width()
+	
+	var scale = viewport_size.x / parallax_texture_width
+	var result = Vector2(scale, scale)
+	return result
+	
+func setup_parallax_layer(parallax_layer: ParallaxLayer):
+	var parallax_sprite = parallax_layer.find_child("Sprite2D")
+	if parallax_sprite:
+		parallax_sprite.scale = get_parallax_sprite_scale(parallax_sprite)
+		var mx = parallax_sprite.scale.x * parallax_sprite.get_texture().get_height()
+		parallax_layer.motion_mirroring.y = mx
+		
+		print(parallax_sprite.scale)
+		print(parallax_layer.motion_mirroring.y)
+
+func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	if Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
-
-
-func create_platorm(location: Vector2):
-	var platform = platform_scene.instantiate()
-	platform.global_position = location
-	platform_parent.add_child(platform)
-	return platform
 	
+
+func new_game():
+	player = player_scene.instantiate()
+	player.global_position = player_spawn_pos
+	add_child(player)
+	
+	camera = camera_scene.instantiate()
+	camera.setup_camera(player)
+	add_child(camera)
+	
+	if player:
+		level_generator.setup(player)
